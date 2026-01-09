c24dac0001abb317641e95e966e9cb3c3c90af2b
TubeView <122611999+TubeView@users.noreply.github.com>
2026-01-09 08:30:18 +0100

    First Push
    Created the game file and started the project. I also imported all files I am going to use.

---
+++ UnityOmexamination/Assets/Scripts-20260109T072336Z-1-001/Scripts/Dialogue/Brackley/DialogueManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TMP_Text nameField;
+    public TMP_Text dialogueField;
+    
+    private Queue<string> sentences;
+
+    public Animator animator;
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(DialogueBrackley dialogueBrackley)
+    {
+        animator.SetBool("isOpen", true);
+        
+        nameField.text = dialogueBrackley.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogueBrackley.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    private IEnumerator TypeSentence(string sentence)
+    {
+        dialogueField.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueField.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    private void EndDialogue()
+    {
+        StopAllCoroutines();
+        
+        animator.SetBool("isOpen", false);
+        Debug.Log("Ending conversation");
+    }
+}
